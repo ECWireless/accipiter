e12e64edc6734c1e4bd81f358e37a5d04c3028b3
ECWireless <Elliott@valtechcreative.com>
2021-05-22 15:54:46 -0600

    Add cookie functionality

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,27 +1,34 @@
 import { useRouter } from 'next/router';
+import { Magic } from 'magic-sdk'
 
 import Spacer from 'components/Spacer';
 
 const Login: React.FC = () => {
   const router = useRouter()
+
   const handleSubmit = async (event) => {
     event.preventDefault()
 
     const { elements } = event.target
 
-    // Add the Magic code here
+    const did = await new Magic(process.env.MAGIC_PUBLISHABLE_KEY)
+      .auth
+      .loginWithMagicLink({ email: elements.email.value })
 
     // Once we have the token from magic,
     // update our own database
-    
-    // const authRequest = await fetch()
 
-    // if (authRequest.ok) {
+    const authRequest = await fetch('/api/login', {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${did}` }
+    })
+  
+    if (authRequest.ok) {
       // We successfully logged in, our API
       // set authorization cookies and now we
       // can redirect to the dashboard!
-      // router.push('/dashboard')
-    // } else { /* handle errors */ }
+      router.push('/portal')
+    } else { /* handle errors */ }
   }
 
   return (
--- frontend/pages/portal.tsx
+++ frontend/pages/portal.tsx
@@ -1,7 +1,36 @@
+import { useRouter } from 'next/router';
+
+import useAuth from 'hooks/useAuth';
+
+import Spacer from 'components/Spacer';
+
 const Portal: React.FC = () => {
+  const router = useRouter()
+  const { user, loading } = useAuth();
+
+  const onLogout = async () => {
+    if (!user) return;
+    
+    const authRequest = await fetch('/api/logout', {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${user.publicAddress}` }
+    })
+  
+    if (authRequest.ok) {
+      // We successfully logged in, our API
+      // set authorization cookies and now we
+      // can redirect to the dashboard!
+      router.push('/')
+    } else { /* handle errors */ }
+  }
+
   return (
     <div>
-      Portal
+      <Spacer size={'lg'} />
+      <h1>Portal</h1>
+      {loading ? "Loading..." : user.email}
+      <Spacer size={'md'} />
+      <button onClick={onLogout}>Logout</button>
     </div>
   )
 }

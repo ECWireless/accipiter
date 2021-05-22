4dad2f4179f4a0e91ba2c560513523367dc4838a
ECWireless <Elliott@valtechcreative.com>
2021-05-22 13:31:26 -0600

    Add magic libs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   env: {
       SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
       SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+      MAGIC_PUBLISHABLE_KEY: process.env.MAGIC_PUBLISHABLE_KEY,
+      MAGIC_SECRET_KEY: process.env.MAGIC_SECRET_KEY,
   },
 }

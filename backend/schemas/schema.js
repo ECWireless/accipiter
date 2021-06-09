17b05e13daf25c53061c3479da5e62d8206a91b0
ECWireless <Elliott@valtechcreative.com>
2021-06-08 20:04:16 -0600

    Remove unnecssary rows in Sanity dashboard

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -25,9 +25,9 @@ export default createSchema({
     // in the studio.
     home,
     careers,
-    post,
-    author,
-    category,
+    // post,
+    // author,
+    // category,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,

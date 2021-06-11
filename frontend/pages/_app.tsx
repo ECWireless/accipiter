dac2ff4bbc8b09f4738cffbd9bc6ecb87c2d49b1
ECWireless <Elliott@valtechcreative.com>
2021-06-11 15:33:21 -0600

    Improve SEO

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -15,10 +15,31 @@ export default function App({ Component, pageProps }): JSX.Element {
 		<>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" href="/favicon.ico" />
+
         <meta name="description" content="Accipiter Systems provides low latency, high bandwidth switching & networking for next generation applications." />
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="theme-color" content="#000000" />
+
+				<meta property="og:title" content="Accipiter Systems | United States" />
+				<meta property="og:description" content="Accipiter Systems provides low latency, high bandwidth switching & networking for next generation applications." />
+				<meta property="og:image" content="https://media-exp3.licdn.com/dms/image/C4D0BAQGwj1Yn6c-Htw/company-logo_200_200/0/1519922119076?e=2159024400&v=beta&t=v6KCaeoyQNwzZLUvlssIsaE9oMWW9TiWD1h4JZjE9hQ" />
+				<meta property="og:type" content="website" />
+				<meta property="og:site_name" content="Accipiter Systems" />
+				<meta property="og:url" content="https://accipitersystems.com/" />
+
+				<meta name="twitter:title" content="Accipiter Systems | United States" />
+				<meta name="twitter:description" content="Accipiter Systems provides low latency, high bandwidth switching & networking for next generation applications." />
+				<meta name="twitter:card" content="https://media-exp3.licdn.com/dms/image/C4D0BAQGwj1Yn6c-Htw/company-logo_200_200/0/1519922119076?e=2159024400&v=beta&t=v6KCaeoyQNwzZLUvlssIsaE9oMWW9TiWD1h4JZjE9hQ" />
+				<meta name="twitter:image" content="https://media-exp3.licdn.com/dms/image/C4D0BAQGwj1Yn6c-Htw/company-logo_200_200/0/1519922119076?e=2159024400&v=beta&t=v6KCaeoyQNwzZLUvlssIsaE9oMWW9TiWD1h4JZjE9hQ" />
+				
+				<meta name="keywords" content="accipiter, systems, bandwidth, switching, networking, application, technology, automation, storage, HPC, development" />
+				<meta name="robots" content="index, follow" />
+				<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+				<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+				<link rel="canonical" href="https://accipitersystems.com/"></link>
+
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />			</Head>
 			

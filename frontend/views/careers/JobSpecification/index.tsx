7473bcd7da77be544fc35ffbf7688a7258e31a19
ECWireless <elliott@coopallc.com>
2023-04-23 15:38:14 -0600

    Update careers page

--- frontend/views/careers/JobSpecification/index.tsx
+++ frontend/views/careers/JobSpecification/index.tsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import { Fade } from "react-reveal";
-import BlockContent from "@sanity/block-content-to-react";
-import { colors, GU } from "components/theme";
+// import BlockContent from "@sanity/block-content-to-react";
+import {
+  colors,
+  // GU
+} from "components/theme";
 
 import Banner from "components/Banner";
 import { Container } from "components/Containers";
-import { H3, P2Block } from "components/Typography";
+import {
+  // H3, P2Block,
+  P2,
+} from "components/Typography";
 import Spacer from "components/Spacer";
 
 interface IJobSpecification {
@@ -22,18 +28,35 @@ interface IJobSpecification {
 export const JobSpecification: React.FC<IJobSpecification> = ({
   heading,
   subheading,
-  position1Title,
-  position1Description,
-  position2Title,
-  position2Description,
-  position3Title,
-  position3Description,
+  // position1Title,
+  // position1Description,
+  // position2Title,
+  // position2Description,
+  // position3Title,
+  // position3Description,
 }) => {
   return (
     <div>
       <Banner heading={heading} subheading={subheading} />
       <Container>
         <Fade bottom ssrFadeout>
+          <P2>
+            Accipiter Systems develops computer networking products that bring
+            PCIe “out of the box”. Our products accelerate Edge applications and
+            systems by eliminating legacy network protocols for networking. Low
+            latency, high throughput, reduced cost of ownership, and enhanced
+            security are the benefits of PCIe networking technology.
+          </P2>
+        </Fade>
+        <Spacer size={"md"} />
+        <Fade bottom delay={100} ssrFadeout>
+          <P2>
+            We’re always on the lookout for talented team members. Check back
+            for updates, or submit your information using the contact form
+            below.
+          </P2>
+        </Fade>
+        {/* <Fade bottom ssrFadeout>
           <H3 bold={true}>{position1Title}</H3>
         </Fade>
         <Spacer size={"md"} />
@@ -53,7 +76,7 @@ export const JobSpecification: React.FC<IJobSpecification> = ({
           <P2Block>
             <BlockContent blocks={position2Description} />
           </P2Block>
-        </Fade>
+        </Fade> */}
         {/* <Spacer size={"md"} />
         <StyledLine />
         <Spacer size={"md"} />
